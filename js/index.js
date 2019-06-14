0c5fba01718d37aa9b59685816af0669533e6fcf
prezes2018 <szczuk2@interia.pl>
2019-06-14 22:00:08 +0200

    move items (tasks) / fix style bugs
    created drag and drop of the tasks, rotating, placeholder

--- js/index.js
+++ js/index.js
@@ -5,6 +5,21 @@
 
     function init() {
 
+        $(".column-items").sortable({
+            connectWith: '.column-items',
+            placeholder: 'placeholder-item',
+            zIndex: 9999,
+            start: function( e, ui ){
+                    e.stopPropagation();
+                    ui.helper[0].classList.add('rotate');
+                    ui.placeholder.height(ui.helper[0].clientHeight)
+                },
+                stop: function( e, ui ) {
+                    e.stopPropagation();
+                    ui.item[0].classList.remove("rotate");
+                }
+        });
+
         // move the entire board
         $("#container").sortable({
             zIndex: 9999,
@@ -17,7 +32,6 @@
                     ui.item[0].classList.remove("rotate");
                 }
         });
-
     }
 
 })();
